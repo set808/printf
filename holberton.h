b839a5732fbc9d8fbba9eb5ddaf577ca209dd171
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-24 18:46:27 +0000

    Add files to get identifier function
    Created get_id_func and id_t typedeef struct to find the proper print function
    for _printf.

--- holberton.h
+++ holberton.h
@@ -1,6 +1,23 @@
 #ifndef _PRINTF
 #define _PRINTF
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * identifier - definition of _printf identifier
+ * @id: the identifier
+ * @f: function for identifier
+ *
+ */
+typedef struct identifier
+{
+	char *id;
+	void (*f)(va_list);
+}id_t;
 
 int _printf(const char *format, ...);
+void (*get_id_func)(char *id)(va_list);
+int _strcmp(char *s1, char *s2);
 
 #endif /* _PRINTF */
