54f88980ba81cbc743f1a7b9251d0f28886004e2
Kevin Yook <yook00627@gmail.com>
2018-02-25 00:10:55 +0000

    create the main printf function
    the main printf function inludes all the function calles required to print the arguments
    in this print f function it includes a creation of buffer of size 1024 and
    it will write buffer to print out the strings we pass it

---
+++ printf.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * _printf - prints all the characters and arguments passed to it
+ * @format: all the arguments passed depending on identifiers
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int buf_c = 0, form_c = 0, s_c = 0;
+	char *buffer, *str, *id;
+
+	va_start(arg, format);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (0);
+	while (format[form_c] != '\0')
+	{
+		if (format[form_c] == '%')
+		{
+			id = find_id(format, form_c);
+			str = (*get_id_funct(id))(arg);
+			while (str[s_c] != '\0')
+			{
+				buffer[buf_c] = str[s_c];
+				buf_c++;
+				s_c++;
+			}
+			form_c += (s_c - 1);
+		}
+		else
+			buffer[buf_c] = format[form_c];
+		buf_c++;
+		form_c++;
+	}
+	write(1, buffer, buf_c);
+	va_end(arg);
+	return (buf_c);
+}
