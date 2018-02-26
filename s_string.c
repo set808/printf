8c96706787e501d84c17774193b19be76592541b
Kevin Yook <yook00627@gmail.com>
2018-02-26 22:05:54 +0000

    revert s_string funciton back to normal state

--- s_string.c
+++ s_string.c
@@ -9,11 +9,17 @@
  */
 char *string_to_string(va_list arg)
 {
-	char *s, *c_s;
+	char *s, *c_s, *n;
 
 	s = va_arg(arg, char *);
 	if (s == NULL)
-		return (NULL);
+	{
+		n = malloc(sizeof(char) * 7);
+		if (n == NULL)
+			return (NULL);
+		n = strcpy(n, "(null)");
+		return (n);
+	}
 	c_s = malloc(sizeof(char) * (_strlen(s) + 1));
 	if (c_s == NULL)
 		return (NULL);
